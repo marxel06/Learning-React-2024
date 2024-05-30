f25fea74d7036e2a15ae37c253c63f29c3b3a7d7
marxel06 <marxel-mg@hotmail.com>
2024-05-30 13:38:48 -0300

    .

---
+++ curso midulive- mouse-follower/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+function App() {
+  useEffect(()=>{
+    console.log("efecto");
+  })
+  return (
+    <>
+      <h3>proyecto 3</h3>
+
+    </>
+  )
+}
+
+export default App
