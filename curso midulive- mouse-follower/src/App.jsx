2d7534f30b0e4ece3bc6662a72a1125f9d46a4af
marxel06 <marxel-mg@hotmail.com>
2024-05-30 15:15:51 -0300

    update learning useEffect

--- curso midulive- mouse-follower/src/App.jsx
+++ curso midulive- mouse-follower/src/App.jsx
@@ -1,14 +1,55 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-function App() {
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
+  
   useEffect(()=>{
-    console.log("efecto");
-  })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    
+    if(enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  },[enabled])
+
   return (
     <>
-      <h3>proyecto 3</h3>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -25,
+      top: -25,
+      width: 50,
+      height: 50,
+      transform: `translate(${position.x}px, ${position.y}px)`
+    }}
+    />
+    <button onClick={() => setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+    </button>
+  </>
+  )
+}
 
-    </>
+function App() {
+  return (
+
+    <main>
+      <FollowMouse/>
+    </main>
   )
 }
 
